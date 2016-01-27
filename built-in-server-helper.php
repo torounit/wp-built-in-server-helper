d819b69aba7d3896995322bd357ca4a17af92dc9
Hiroshi Urabe <mail@torounit.com>
2016-01-28 05:05:09 +0900

    version fix. codeformatting

--- built-in-server-helper.php
+++ built-in-server-helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Built-in Server Helper
- * Version: 0.0.1
+ * Version: 1.0.0
  * Description: Helper for development WordPress Site with PHP built-in server.
  * Author: Toro_Unit
  * Author URI: http://torounit.com
@@ -13,14 +13,14 @@
 
 
 function built_in_server_helper_notices() {
-    echo '<div class="error"><p>[Built-in Server Helper] Built-in Server Helper do not support multisite.</p></div>';
+	echo '<div class="error"><p>[Built-in Server Helper] Built-in Server Helper do not support multisite.</p></div>';
 }
 
 
-if( is_multisite() ) {
+if ( is_multisite() ) {
 
-    add_action( 'admin_notices', 'built_in_server_helper_notices' );
+	add_action( 'admin_notices', 'built_in_server_helper_notices' );
 
-}else if ( version_compare( phpversion(), '5.4', '>=' ) ) {
-    require dirname( __FILE__ ) . '/src/bootstrap.php';
+} else if ( version_compare( phpversion(), '5.4', '>=' ) ) {
+	require dirname( __FILE__ ) . '/src/bootstrap.php';
 }
